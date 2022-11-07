95fc72c55f002be0ddfc39f78490d2ff8946970b
Silja Tirronen <silja.tirronen@robu.fi>
2022-11-07 11:23:05 +0200

    Add health check end point

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/health')
+def health():
+    return 'hello, silja!'
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080, debug=False)
