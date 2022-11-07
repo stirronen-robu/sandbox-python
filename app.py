2e70998840a7f9d20cecf2ac63b9a9d9c87366d7
Silja Tirronen <silja.tirronen@robu.fi>
2022-11-07 13:41:27 +0200

    Allow CORS on all routes

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template
-from flask_cors import cross_origin
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/health')
-@cross_origin()
 def health():
     return 'hello, silja!'
 
